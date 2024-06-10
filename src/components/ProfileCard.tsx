234b2057c4169bb9ceed6e58ff053df95ba27970
Alexa Schreffler <alexaschreffler@alexas-mac-mini.lan>
2024-06-10 13:31:03 -0400

    added icon svgs, positioned icons

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,3 +1,8 @@
+import Github from "./svgs/Github";
+import Twitter from "./svgs/Twitter";
+import Instagram from "./svgs/Instagram";
+import LinkedIn from "./svgs/LinkedIn";
+
 interface ProfileCardProps {
     avatar: string;
     name: string;
@@ -8,17 +13,25 @@ interface ProfileCardProps {
 
 const ProfileCard = ({ avatar, name, header, bio, contact }: ProfileCardProps) => {
     return (
-        <div className="flex flex-col items-center gap-6 rounded-lg mt-[200px] w-[340px] bg-white">
+        <div className="flex flex-col items-center gap-6 rounded-lg border-2 border-neutral-300 mt-[200px] p-4 w-[340px] bg-white">
             <img className="h-[60px] mt-6" src={avatar} />
             <div className="text-center">
                 <p className="text-2xl">{name}</p>
-                <p className="text-neutral-500">{header}</p>
+                <p className="text-neutral-500 text-sm">{header}</p>
             </div>
             <div className="text-center text-neutral-600">
                 <p>{bio}</p>
             </div>
-            <div>
-                <p>{contact}</p>
+            <div className="w-full">
+                <button className="bg-indigo-700 w-full h-11 hover:bg-indigo-800 shadow-sm text-white py-2 px-4 rounded">
+                    {contact}
+                </button>
+            </div>
+            <div className="flex flex-row justify-around mt-2 mb-3 h-5 w-8/12">
+                <Github />
+                <LinkedIn />
+                <Instagram />
+                <Twitter />
             </div>
         </div>
     )
